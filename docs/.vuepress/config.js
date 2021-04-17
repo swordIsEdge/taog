17025f446a8133f59d432fbfead6c8758df40848
kasumi <zhaozijianhb@163.com>
2021-04-18 01:12:37 +0800

    init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+        // Please keep looking down to see the available options.
+    }
+}
---
+++ docs/_posts/te.md
@@ -0,0 +1,10 @@
+---
+title: dytre
+date: 2020-01-11
+tags: 
+  - JavaScript
+  - Vue
+author: Billyyyyy3320
+location: Taipei  
+---
+# 实施新文件
